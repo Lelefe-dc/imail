391ec2cb515f1832b6258b411a70b2361c6814d6
Lelefe-dc <lelefadebtcollection@gmail.com>
2026-09-05 10:31:41 +0200

    Add Avalonia desktop startup

---
+++ desktop/IMail.Desktop/Program.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.ReactiveUI;
+
+namespace IMail.Desktop;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+        .UsePlatformDetect()
+        .WithInterFont()
+        .LogToTrace();
+}
